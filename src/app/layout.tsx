2c21f0cf990ec7d7ce60e828d77713e68cee6210
youssefshoukry1 <youssefshoukry1@hotmail.com>
2025-09-15 17:05:50 +0300

    update project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
-        {children}
+        {chidren}
         <Footer/>
       </body>
     </html>
